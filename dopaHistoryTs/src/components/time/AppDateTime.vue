bd067e569c97d5b04250f622ddd953e248a0669b
陈泽豪 <chenzehao@chenzehaodeMBP.home>
2024-01-09 22:06:44 +0100

    to ts project

---
+++ dopaHistoryTs/src/components/time/AppDateTime.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="date-time-box">
+
+        <div class="date">
+            <div>{{ fullDate }}</div>
+        </div>
+        <div class="day">
+            {{ dayNames[dateNow.getDay()] }}
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup(){
+
+    },
+    props: {
+        dateNow:{
+            type:Date,
+            required: true
+        }
+    },
+    data() {
+        return {
+            dateIterval: null,
+            dayNames: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+            //dateNow:new Date()
+        }
+    },
+    created() {
+        //this.dateNow = new Date()
+        //this.startDateIterval()
+    },
+    methods: {
+        setDateNow() {
+            //this.dateToday= new Date();
+        },
+        convertToTwoDigit(number: number) {
+            return number < 10 ? '0' + number.toString() : number.toString();
+        },
+
+    },
+    computed: {
+
+        fullDate() {
+            let date = this.convertToTwoDigit(this.dateNow.getDate());
+            let month = this.convertToTwoDigit(this.dateNow.getMonth() + 1)
+            let year = this.dateNow.getFullYear()
+            return `${date} / ${month} / ${year}`
+        },
+        fullTime() {
+            let hours = this.convertToTwoDigit(this.dateNow.getHours());
+            let minutes = this.convertToTwoDigit(this.dateNow.getMinutes());
+            let seconds = this.convertToTwoDigit(this.dateNow.getSeconds());
+            return `${hours} : ${minutes} : ${seconds}`
+        }
+    }
+
+})
+</script>
+<style scoped>
+.date-time-box {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+    font-size: 24px;
+}
+</style>
---
+++ dopaHistoryTs/src/views/HomePage.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <app-date-time :dateNow="dateNow"></app-date-time>
+        <dopa-case></dopa-case>
+    </div>
+
+</template>
+  
+<script lang="ts">
+
+
+import AppDateTime from '../components/time/AppDateTime.vue';
+import DopaCase from '../components/dopaItme/DopaCase.vue'
+//new Date('2024-01-05T23:59:55'),
+export default {
+    components: {
+        AppDateTime,
+        DopaCase,
+    },
+    data() {
+        return {
+            //dateNow: new Date('1995-12-17T23:59:55') as any,
+            dateNow: new Date() as any,
+            nextDate: null as any,
+            restMilliSecondsToNextDay: 0,
+            dateIterval: null as any,
+        };
+    },
+    created() {
+        this.calRestMilliSecondsToNextDay()
+        //set check interval
+        this.setDateIterval()
+
+    },
+    methods: {
+        setDateIterval() {
+            // set date check interval
+            console.log('set date check intervel')
+            if (this.dateIterval) {
+                //console.log('clear old interval')
+                clearInterval(this.dateIterval)
+            }
+
+            let element = this;
+            this.dateIterval = setInterval(function () {
+                element.dateNow = new Date()
+                //console.log('interval')
+                element.setUpNextDay()
+            }, this.restMilliSecondsToNextDay);
+
+        },
+        calNextDate(actualDate: any) {
+
+            let nextDatelocal = new Date(actualDate.getTime())
+            nextDatelocal.setTime(actualDate.getTime() + (24 * 60 * 60 * 1000));
+            nextDatelocal.setHours(0)
+            nextDatelocal.setMinutes(0)
+            nextDatelocal.setSeconds(0)
+            //console.log(nextDatelocal.toString())
+            return nextDatelocal
+        },
+        calRestMilliSecondsToNextDay() {
+            //calulate next day
+            this.nextDate = this.calNextDate(this.dateNow)
+            this.restMilliSecondsToNextDay = this.nextDate.getTime() - this.dateNow.getTime()
+            console.log(this.restMilliSecondsToNextDay)
+        },
+        setUpNextDay() {
+            this.calRestMilliSecondsToNextDay()
+            this.setDateIterval()
+            console.log("new day")
+        }
+    },
+    computed: {
+
+    },
+};
+</script>
