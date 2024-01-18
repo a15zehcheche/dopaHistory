afe4eaf6cf8445a3b23576ef906592b70d6df87f
陈泽豪 <chenzehao@chenzehaodeMBP.home>
2024-01-19 00:05:07 +0100

    commit page construction info

--- dopaHistoryTs/src/views/CommentPage.vue
+++ dopaHistoryTs/src/views/CommentPage.vue
@@ -1,7 +1,11 @@
 <template>
     <base-layout pageTitle="记录">
-    count {{counterStore.count}}
-    <button @click="counterStore.increment()">add</button>
+    <!--count {{counterStore.count}}
+    <button @click="counterStore.increment()">add</button-->
+        <div class="comment-body">
+            <img width="150px" height="150px" src="/assets/under-construction.png"  alt="">
+            <span class="info" >页面施工中。。。</span>
+        </div>
     </base-layout>
 </template>
 <script lang="ts" setup>
@@ -11,3 +15,17 @@ const counterStore = useCounterStore()
 
 
 </script>
+<style lang="less">
+.comment-body{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .info{
+        margin-top: 20px;
+        color: var(--ion-color-medium);
+    }
+}
+</style>
