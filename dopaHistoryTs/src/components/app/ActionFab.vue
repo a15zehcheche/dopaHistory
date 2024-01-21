1dfb05c116b5326b0a35db48791bffdc24fe03d4
陈泽豪 <chenzehao@chenzehaodeMBP.home>
2024-01-22 00:49:59 +0100

    add new doapcase action

---
+++ dopaHistoryTs/src/components/app/ActionFab.vue
@@ -0,0 +1,49 @@
+<template>
+  <ion-fab slot="fixed" horizontal="center" vertical="center">
+    <ion-fab-button>
+      <ion-icon :icon="add"></ion-icon>
+    </ion-fab-button>
+    <ion-fab-list side="top">
+      <ion-fab-button @click="addComment()">
+        <ion-icon src="/assets/comment-alt-edit.svg"></ion-icon>
+      </ion-fab-button>
+    </ion-fab-list>
+    <ion-fab-list side="start">
+      <ion-fab-button color="danger" @click="dopaDo(-1)">
+        <ion-icon :icon="chevronDown"></ion-icon>
+      </ion-fab-button>
+      <ion-fab-button color="danger" @click="dopaDo(1)">
+        <ion-icon :icon="chevronUp"></ion-icon>
+      </ion-fab-button>
+    </ion-fab-list>
+    <ion-fab-list side="end">
+      <ion-fab-button color="warning" @click="dopaThink(1)">
+        <ion-icon :icon="chevronUp"></ion-icon>
+      </ion-fab-button>
+      <ion-fab-button color="warning" @click="dopaThink(-1)">
+        <ion-icon :icon="chevronDown"></ion-icon>
+      </ion-fab-button>
+    </ion-fab-list>
+  </ion-fab>
+</template>
+  
+<script lang="ts" setup>
+import { IonFab, IonFabButton, IonFabList, IonIcon } from '@ionic/vue';
+import { add, chevronBack, chevronDown, chevronForward, chevronUp } from 'ionicons/icons';
+import { defineComponent } from 'vue';
+import { useMySqliteStore } from '@/stores/sqlite'
+const SqliteStore = useMySqliteStore()
+
+const dopaDo = async (n: number) => {
+  SqliteStore.dopaDo(n)
+}
+
+const dopaThink = async (n: number) => {
+  SqliteStore.dopaThink(n)
+}
+
+const addComment = () => {
+  console.log('add comment')
+}
+
+</script>
