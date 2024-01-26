a57b9c19e1f37a7aeeb0071ec1c7bf8c0523202a
陈泽豪 <chenzehao@chenzehaodeMBP.home>
2024-01-26 02:08:12 +0100

    clear old project

--- dopahistoryApp/android/app/src/main/java/com/dopahistory/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.dopahistory.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- dopahistoryApp/setBuildEven.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo 'set even'
-source ~/.bash_profile
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home"
